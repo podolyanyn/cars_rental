f0e77b719ce799f4b01066d196fa6f3b63b01458
podolyanyn <podolyanyn@gmail.com>
2020-03-27 11:11:06 +0200

    Добавив в файли посилання на модуль polls, а також включив сам модуль polls. Зручно, щоб був модуль для тестування

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	#'polls.apps.PollsConfig',
+	'polls.apps.PollsConfig',
 	'cars_rental.apps.CarsRentalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
