ca4aca0b4698b1bc93cd9c56a0fdd0bf7d2b2ec7
podolyanyn <podolyanyn@gmail.com>
2020-04-01 16:32:42 +0300

    Забрав з адмінки об'єкти, пов'язані з Одесою

--- cars_rental/admin.py
+++ cars_rental/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Client, ClientOdesa, Investor, InvestorOdesa, Car, CarOdesa, ClientContract, ClientContractOdesa, InvestorContract, InvestorContractOdesa
+from .models import Client, Investor, Car, ClientContract, InvestorContract
+#from .models import ClientOdesa, InvestorOdesa, CarOdesa, ClientContractOdesa, InvestorContractOdesa
+
 #from .models import ClientOdesa
 #from .models import Investor
 #from .models import InvestorOdesa
@@ -13,12 +15,12 @@ from .models import Client, ClientOdesa, Investor, InvestorOdesa, Car, CarOdesa,
 #from .models import InvestorContractOdesa
 
 admin.site.register(Client)
-admin.site.register(ClientOdesa)
+#admin.site.register(ClientOdesa)
 admin.site.register(Investor)
-admin.site.register(InvestorOdesa)
+#admin.site.register(InvestorOdesa)
 admin.site.register(Car)
-admin.site.register(CarOdesa)
+#admin.site.register(CarOdesa)
 admin.site.register(ClientContract)
-admin.site.register(ClientContractOdesa)
+#admin.site.register(ClientContractOdesa)
 admin.site.register(InvestorContract)
-admin.site.register(InvestorContractOdesa)
+#admin.site.register(InvestorContractOdesa)
