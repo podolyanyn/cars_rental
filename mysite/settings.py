e87ef9d7591e41dfaaf41b5066f697a84039a874
podolyanyn <podolyanyn@gmail.com>
2020-06-23 19:50:00 +0300

    Перемикання БД з SQLite на MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-		#'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-		#'NAME': 'cars_rental',
-		#'USER': 'root',
-        #'PASSWORD': 'root',
-        #'HOST': '127.0.0.1',
-        #'PORT': '3306',
+        #'ENGINE': 'django.db.backends.sqlite3',
+		'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'NAME': 'cars_rental',
+		'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
 		
     }
 }
